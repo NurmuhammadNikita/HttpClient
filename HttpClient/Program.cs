281686a4f803b1312866f247c091212496e306e2
NurmuhammadNikita <888legolas777@gmail.com>
2023-10-05 10:45:02 +0500

    Добавьте файлы проекта.

---
+++ HttpClient/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HttpCRUD;
+
+Console.WriteLine("Hello, World!");
+HttpCrudMethod http = new HttpCrudMethod();
+string result;
+//result = await http.GetAllAsync();
+
+//await http.PostAsync("Kitchen");
+//await http.GetAllAsync();
+await http.PutAsync(3, "Garment");
+//result = await http.PostAsync();
+//result = await http.DeleteAsync(4);
